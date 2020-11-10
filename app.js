811f22f72d353091fe7a763bba842ce225d2eb3e
Elizaveta <lizayakovleva2005@gmail.com>
2020-11-10 19:09:28 +0200

    added all

---
+++ app.js
@@ -0,0 +1,13 @@
+// Addition Subtraction Multiplication Division
+let a = prompt("Enter variable a");
+let b = prompt("Enter variable b");
+a = Number(a);
+b = Number(b);
+alert(a);
+alert(b);
+let sum = a + b;
+let subtraction = a - b;
+let composition = a * b;
+let division = a / b;
+
+alert(" Sum = " + sum + " subtraction = " + subtraction + " composition = " + composition + " division = " + division);
